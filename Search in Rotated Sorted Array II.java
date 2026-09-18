9919c1cd5ed3b6a4dcda5865d5b69844e3a4f56e
vidishasrivastava142-maker <vidishasrivastava142@gmail.com>
2026-09-18 11:59:50 +0530

    [vidisha srivastava]Add.programs

---
+++ Search in Rotated Sorted Array II.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+
+        int left = 0;
+        int right = n - 1;
+        boolean found = false;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                found = true;
+                break;
+            }
+
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+            } else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        System.out.println(found);
+    }
+}
